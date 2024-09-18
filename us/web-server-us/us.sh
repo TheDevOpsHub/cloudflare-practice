abd745e219559974ea2c8f45a7973057710d3f0d
Tung Bui Quang (Leo) <tung.bquang@gmail.com>
2024-09-18 14:12:30 +0700

    Cmd for ubuntu

--- us/web-server-us/us.sh
+++ us/web-server-us/us.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum update -y
-sudo amazon-linux-extras install nginx1 -y
-echo "Welcome to the US infrastructure!" > /usr/share/nginx/html/index.html
+sudo apt update -y
+sudo apt install nginx -y
+echo "Welcome to the US infrastructure!" | sudo tee /var/www/html/index.html
 sudo systemctl start nginx
 sudo systemctl enable nginx
--- vn/web-server-vn/vn.sh
+++ vn/web-server-vn/vn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum update -y
-sudo amazon-linux-extras install nginx1 -y
-echo "Welcome to the Vietnam infrastructure!" > /usr/share/nginx/html/index.html
+sudo apt update -y
+sudo apt install nginx -y
+echo "Welcome to the Vietnam infrastructure!" | sudo tee /var/www/html/index.html
 sudo systemctl start nginx
 sudo systemctl enable nginx
